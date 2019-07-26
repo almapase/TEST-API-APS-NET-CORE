f589497bf3894193ffac46cca4a643f55c801ba0
Alex Pacheco <alex.pacheco.s@gmail.com>
2019-07-26 16:58:58 -0400

    Lista la autenticación por token

---
+++ Controllers/AuthoizeAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PaisesAPI.Controllers
+{
+    internal class AuthoizeAttribute : Attribute
+    {
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using PaisesAPI.Models;
 
 namespace PaisesAPI
@@ -34,6 +37,20 @@ namespace PaisesAPI
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options => options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "yourdomain.com",
+                    ValidAudience = "yourdomain.com",
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(Configuration["Llave_super_secreta"])),
+                    ClockSkew = TimeSpan.Zero
+                });
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2).AddJsonOptions(ConfigureJson);
         }
 
@@ -55,27 +72,28 @@ namespace PaisesAPI
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
 
-            //if (!context.Paises.Any())
-            //{
-            //    context.Paises.AddRange(new List<Pais>()
-            //    {
-            //        new Pais(){Nombre = "Chile", Provincias = new List<Provincia>(){
-            //                new Provincia(){Nombre = "Santiago" },
-            //                new Provincia(){Nombre = "Concepción"}
-            //            }
-            //        },
-            //        new Pais(){Nombre = "Peru", Provincias = new List<Provincia>(){
-            //                new Provincia(){Nombre = "Cusco" },
-            //                new Provincia(){Nombre = "Arequipa"}
-            //            }
-            //        },
-            //        new Pais(){Nombre = "Argentina"}
-            //    });
-            //    context.SaveChanges();
-            //}
+            if (!context.Paises.Any())
+            {
+                context.Paises.AddRange(new List<Pais>()
+                {
+                    new Pais(){Nombre = "Chile", Provincias = new List<Provincia>(){
+                            new Provincia(){Nombre = "Santiago" },
+                            new Provincia(){Nombre = "Concepción"}
+                        }
+                    },
+                    new Pais(){Nombre = "Peru", Provincias = new List<Provincia>(){
+                            new Provincia(){Nombre = "Cusco" },
+                            new Provincia(){Nombre = "Arequipa"}
+                        }
+                    },
+                    new Pais(){Nombre = "Argentina"}
+                });
+                context.SaveChanges();
+            }
         }
     }
 }
